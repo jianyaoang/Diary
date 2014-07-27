1ba51f7126d0beea841585a88876e8c7a7eec3c9
jianyaoang <jianyaoang1@gmail.com>
2014-07-27 16:25:13 -0500

    adding files

---
+++ Diary/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Diary
+//
+//  Created by Jian Yao Ang on 7/27/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
